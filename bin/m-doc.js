c6513ac1f769e2954be6513636c0b6329b3d85b7
xd <xingdong.com@live.com>
2021-03-09 00:52:02 +0800

    chore: link lib

--- bin/m-doc.js
+++ bin/m-doc.js
@@ -1,13 +1,15 @@
+#!/usr/bin/env node
+
 const { Command } = require('commander')
 const chalk = require('chalk')
-const { startServer } = require('./index')
+const { startServer } = require('../lib')
 const { version } = require('../package.json')
 const program = new Command()
 
 program
-  .option('-p, --post', 'default post: 19364')
-  .option('-host, --host', 'select host address to bind, default: 0.0.0.0 or local ip address')
-  .option('-d, --dist', 'doc folder, default: docs')
+  .option('-p, --port <type>', 'default post: 12149')
+  .option('-host, --host <type>', 'select host address to bind, default: 0.0.0.0 or local ip address')
+  .option('-d, --dist <type>', 'doc folder, default: docs')
   .option('-no-browser, --no-browser', 'prohibit automatic web browser launching')
   .version(`M-Doc ${chalk.green(version)}`, '-v, --version', 'display version')
   .parse(process.argv)
--- src/constants.js
+++
@@ -1,8 +0,0 @@
-const PORT = 19366
-
-const LOCAL_ADDRESS = '0.0.0.0'
-
-module.exports = {
-  PORT,
-  LOCAL_ADDRESS
-}
